5020a56e832338c4c6a20683ba3dde33a1ed521d
Li Bo <bo.li@easystack.cn>
2025-08-14 17:30:06 +0800

    add cursor

---
+++ quanx/rewrite/cursor/cursor.js
@@ -0,0 +1,29 @@
+/*
+{
+  "teamMembershipType" : null,
+  "trialEligible" : true, //试用资格
+  "customerBalance" : null,
+  "membershipType" : "free", //核心会员状态，可能 free/pro/pro_trial
+  "paymentId" : "cus_Srb9DQSFTMzWTd",
+  "isTeamMember" : false,
+  "isOnStudentPlan" : false,
+  "individualMembershipType" : "free", // 个人订阅类型
+  "trialWasCancelled" : false, // 试用是否被取消
+  "verifiedStudent" : false
+}
+
+{"membershipType":"free_trial","daysRemainingOnTrial":6,"verifiedStudent":false,"trialEligible":false,"isOnStudentPlan":false,"customerBalance":null}
+*/
+
+let body = $response.body;
+try {
+    let obj = JSON.parse(body);
+    obj.membershipType = "pro_trial";
+    obj.trialEligible = true;
+    obj.trialWasCancelled = false
+    obj.individualMembershipType = "pro_trial"
+    obj.daysRemainingOnTrial = 14
+    $done({body: JSON.stringify(obj)});
+} catch (e) {
+    $done({body});
+}
