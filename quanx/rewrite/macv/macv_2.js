ab96d1c8538ae0159c58aa27af310abfcb5ccd07
Li Bo <bo.li@easystack.cn>
2025-09-24 22:26:42 +0800

    optimize macv

--- quanx/rewrite/macv/macv_2.js
+++ quanx/rewrite/macv/macv_2.js
@@ -3,10 +3,10 @@ let body = $response.body;
 let jsonBody = JSON.parse(body);
 
 try {
-    jsonBody.code = 200;
+    jsonBody.code = 1000;
     jsonBody.msg = "success";
 } catch (error) {
-    jsonBody.code = 200;
+    jsonBody.code = 1000;
     jsonBody.msg = "success";
     console.log(error);
     $done({ body: body });
