6d88f3d2a930dc79336a0bb9bacdef137691953a
Li Bo <bo.li@easystack.cn>
2026-04-03 20:36:08 +0800

    add ads.conf

--- quanx/rewrite/macv/macv_2.js
+++ quanx/rewrite/macv/macv_2.js
@@ -1,3 +1,5 @@
+// code: 200
+// url: "https://down.mac89.com/202403/K4b8D01995867A03.dmg?token=dd8bec6cc293bfa61073e47a74e9228c1758724311"
 let body = $response.body;
 
 let jsonBody = JSON.parse(body);
